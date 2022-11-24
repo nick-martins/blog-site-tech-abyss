16fcd772aff042235aff6577c0b55179ea4780aa
nickmartins <92471835+nick-martins@users.noreply.github.com>
2022-11-24 15:11:45 -0500

    Remove the contact form and the about me page

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -11,16 +11,8 @@ const MenuItems = [
     title: "Home",
   },
   {
-    path: "/about",
-    title: "About",
-  },
-  {
     path: "/blog",
-    title: "Blog",
-  },
-  {
-    path: "/contact",
-    title: "Contact",
+    title: "Blog Posts",
   },
 ]
 
